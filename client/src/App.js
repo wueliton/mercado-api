3f791dc134d8aa1c1058ad4c75550fa7a6e21f25
Paulo Wuéliton <paulo.wueliton@hotmail.com>
2021-05-02 16:33:46 -0300

    Adicionado React Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,27 @@
 import "./App.css";
 import { UserProvider } from "./contexts/user";
 import TabelaUsersComponent from "./components/TabelaUsers";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { createBrowserHistory } from "history";
+
+const hist = createBrowserHistory();
 
 function App() {
   return (
-    <Router>
-      <UserProvider>
-        <TabelaUsersComponent></TabelaUsersComponent>
-      </UserProvider>
+    <Router history={hist}>
+      <Switch>
+        <Route>
+          <div>Index</div>
+        </Route>
+        <UserProvider>
+          <Route path="/users">
+            <TabelaUsersComponent></TabelaUsersComponent>
+          </Route>
+          <Route path="/user/:id">
+            <div>Teste</div>
+          </Route>
+        </UserProvider>
+      </Switch>
     </Router>
   );
 }
